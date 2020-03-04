24d61fcd3cbf88b1844b0bfb7c750b2cd46ca80d
mtfaye <tahafaye@hotmail.com>
2020-03-04 16:00:02 -0500

    scrapped chats and sms

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,28 +1,22 @@
 
 """
 LOAD THE RAW DATA AND CONVERT TO DATAFRAME FORMAT
- """
 
+ """
 import os
 import re
-import sys
 import json
 import email
+import pickle
 import mailbox
 import pandas as pd
 from time import asctime
-from dateutil.parser import parse
 from bs4 import BeautifulSoup
+from dateutil.parser import parse
 
-# ############################################################ #
-# Converting the raw data folder to a standardized mbox format #
-# ############################################################ #
+from data.directory_root import path_file, MBOX, sms_folder, chats_folder
 
-# path file data
-path_file= '/Users/mouhamethtakhafaye/Desktop/behavox_assignment/data/immutable_input_data'
-
-# Where to write the converted mbox
-MBOX = '/Users/mouhamethtakhafaye/Desktop/behavox_assignment/data/immutable_input_data.mbox'
+# Converting the raw data folder to a standardized mbox format
 
 # Create a file handle that we'll be writing into...
 mbox = open(MBOX, 'w+')
@@ -44,7 +38,7 @@ for (root, dirs, file_names) in os.walk(path_file):
         _date = re.search(r"Date: ([^\r\n]+)", message_text).groups()[0]
 
         # Convert _date to the asctime representation for the From_ line
-       _date = asctime(parse(_date).timetuple())
+        _date = asctime(parse(_date).timetuple())
 
         msg = email.message_from_string(message_text)
         msg.set_unixfrom('From {0} {1}'.format(_from, _date))
@@ -53,30 +47,57 @@ for (root, dirs, file_names) in os.walk(path_file):
 
 mbox.close()
 
+
 # ############################################################ #
-#      Loading the mailbox data into Pandas                    #
+#      Loading emails (Inbox folder only) data                 #
 # ############################################################ #
 
 mbox = mailbox.mbox(MBOX)
 
-mbox_dict = {}
-for i, msg in enumerate(mbox):
-    mbox_dict[i] = {}
-    for header in msg.keys():
-        mbox_dict[i][header] = msg[header]
-        mbox_dict[i]['Body'] = msg.get_payload().replace('\n', ' ').replace('\t', ' ').replace('\r', ' ').strip()
+def load_emails(mbox):
 
-df = pd.DataFrame.from_dict(mbox_dict, orient='index')
+    mbox_dict = {}
+    for i, msg in enumerate(mbox):
+        mbox_dict[i] = {}
+        for header in msg.keys():
+            mbox_dict[i][header] = msg[header]
+            mbox_dict[i]['Body'] = msg.get_payload().replace('\n', ' ').replace('\t', ' ').replace('\r', ' ').strip()
+
+    data_emails = pd.DataFrame.from_dict(mbox_dict, orient='index')
+    return data_emails
 
 
 # ############################################################ #
-#      Loading the sms folder                                  #
+#      Loading the sms data                                    #
 # ############################################################ #
-sms_file = '/Users/mouhamethtakhafaye/Desktop/behavox_assignment/data/immutable_input_data/Communication_samples/sms'
-for filename in os.listdir(p):
-    if filename.endswith('.html'):
-        fname = os.path.join(sms_file,filename)
-        with open(fname, 'r') as f:
-            soup = BeautifulSoup(f.read(),'html.parser')
-            ''.join(soup.findAll(text=True))
 
+def load_sms(sms_folder):
+
+    for filename in os.listdir(sms_folder):
+        if filename.endswith('.html'):
+            fname = os.path.join(sms_folder, filename)
+            with open(fname, 'r') as f:
+
+                soup = BeautifulSoup(open(fname), "html.parser")
+                sms = [p.text for p in soup.findAll("div", class_="text")]
+
+                data_sms = json.dumps(sms, indent=4)
+                return data_sms
+
+
+# ############################################################ #
+#      Loading the chats data                                  #
+# ############################################################ #
+
+def load_chats(chats_folder):
+
+    for filenames in os.listdir(chats_folder):
+        if filenames.endswith('.xml.html'):
+            f_name = os.path.join(chats_folder, filenames)
+            with open(f_name, 'r') as f:
+
+                makesoup = BeautifulSoup(open(f_name), "html.parser")
+                chats = [i.text for i in makesoup.findAll("div", class_="text")]
+
+                data_chats = json.dumps(chats, indent=4)
+                return data_chats
---
+++ src/data/processing_data.py
@@ -0,0 +1,25 @@
+from data.load_data import load_sms, load_emails, load_chats, mbox
+from data.directory_root import path_file, MBOX, sms_folder, chats_folder
+
+print(load_emails(mbox))
+#serialize emails data json
+
+
+#serialize chats data json
+
+
+
+#serialize sms data json
+
+
+
+# Concat all three data to make a Corpus
+
+
+
+# Save file on data folder
+
+
+
+
+
