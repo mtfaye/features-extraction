4ec6c2c9a834918cfddeccaf37a4968b250a5199
mtfaye <tahafaye@hotmail.com>
2020-03-12 02:21:38 -0400

    updated table format on APP.PY

--- src/app.py
+++ src/app.py
@@ -49,8 +49,7 @@ app.layout = html.Div(
                 """This application extracts text across three different channels of communication; emails, 
                 sms and chats. It then performs a statistical modeling technique to find interesting subject. The 
                 table below shows the result. It contains the 15 most important features for each channel. At this 
-                point it becomes our job as a human to interpret the result and after looking at the scores for 
-                each channel, I think these features convey a collusive behavior from the user. What is your thought? 
+                point it becomes our job as a human to interpret the result.  
                 """
             )
         ]
--- tests/test_loading.py
+++ tests/test_loading.py
@@ -9,4 +9,4 @@ import src.data_loading
 class TestLoading(unittest.TestCase):
 
     def test__load_emails__(self):
-        self.
+        self.assertRaises()
